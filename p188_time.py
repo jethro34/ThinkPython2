186714f4aaf642de52457a49f68b495bc807d028
jethro34 <jh.triana@hotmail.com>
2022-03-09 22:48:49 -0500

    v.1 commented

--- p188_time.py
+++ p188_time.py
@@ -1,4 +1,7 @@
+
 class Time:
+    """ Represents a time with hours, minutes, and seconds. """
+
     def __init__(self, h, m, s):
         self.h = h
         self.m = m
@@ -6,10 +9,15 @@ class Time:
 
 
 def print_time(time_obj):
+    """ Prints a time object using the hh:mm:ss format. """
+
     print('%.2d:%.2d:%.2d' % (time_obj.h, time_obj.m, time_obj.s))
 
 
 def is_after(t1, t2):
+    """ Checks if the first of two time objects happens chronologically after the second.
+        Output: Boolean """
+
     return (t1.h > t2.h) or (t1.h == t2.h and t1.m > t2.m) or (t1.h == t2.h and t1.m == t2.m and t1.s > t2.s)
 
 
