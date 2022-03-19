5187ba7d1cb0dedfa9ab843ba22dee675ca4291b
jethro34 <jh.triana@hotmail.com>
2022-03-19 16:37:51 -0400

    v.1

--- p220ex18-3_poker.py
+++ p220ex18-3_poker.py
@@ -106,6 +106,9 @@ def find_defining_class(obj, method_name):
 class PokerHand(Hand):
     """Represents a poker hand."""
 
+    straights = [{1, 2, 3, 4, 5}, {2, 3, 4, 5, 6}, {3, 4, 5, 6, 7}, {4, 5, 6, 7, 8}, {5, 6, 7, 8, 9},
+                 {6, 7, 8, 9, 10}, {7, 8, 9, 10, 11}, {8, 9, 10, 11, 12}, {9, 10, 11, 12, 13}, {10, 11, 12, 13, 1}]
+
     def suit_hist(self):
         """ Builds a histogram of the suits that appear in the hand.
             Stores the result in attribute suits. """
@@ -148,17 +151,15 @@ class PokerHand(Hand):
     def has_straight(self):
         """ Returns True if the hand has five cards with ranks in sequence, False otherwise. """
         self.rank_hist()
-        ranks_in_hand = sorted(self.ranks.keys())
-        if len(ranks_in_hand) > 4:
-            if {1, 10, 11, 12, 13}.issubset(set(ranks_in_hand)):
-                return True
-            for i in range(len(ranks_in_hand) - 4):
-                if ranks_in_hand[i: i + 5][-1] - ranks_in_hand[i: i + 5][0] == 4:
+        if len(self.ranks.keys()) > 4:
+            ranks_in_hand = set(self.ranks.keys())
+            for rank_set in self.straights:
+                if rank_set.issubset(ranks_in_hand):
                     return True
         return False
 
     def has_flush(self):
-        """ Returns True if the hand has five cards with the same suit, False otherwise.
+        """ Returns True if the hand has five cards with the same suit; False otherwise.
             Note that this works correctly for hands with more than 5 cards. """
         self.suit_hist()
         for val in self.suits.values():
@@ -166,21 +167,101 @@ class PokerHand(Hand):
                 return True
         return False
 
-if __name__ == '__main__':
-    # make a deck
-    deck = Deck()
-    deck.shuffle()
+    def has_full_house(self):
+        """ Returns True if the hand has three cards with one rank and two with another; False otherwise. """
+        self.rank_hist()
+        has_three, has_two = False, False
+        for val in self.ranks.values():
+            if val == 3:
+                has_three = True
+            elif val == 2:
+                has_two = True
+        return has_three and has_two
 
-    # deal the cards and classify the hands
-    for i in range(7):
+    def has_four_of_a_kind(self):
+        """ Returns True if the hand has four cards with the same rank, False otherwise. """
+        self.rank_hist()
+        for val in self.ranks.values():
+            if val == 4:
+                return True
+        return False
+
+    def has_straight_flush(self):
+        """ Returns True if the hand has five consecutive cards with the same suit; False otherwise. """
+        if self.has_flush():
+            max_suit = max(self.suits, key=self.suits.get)
+            same_suit_ranks_in_hand = {card.rank for card in self.cards if card.suit == max_suit}
+            for rank_set in self.straights:
+                if rank_set.issubset(same_suit_ranks_in_hand):
+                    return True
+        return False
+
+    def classify(self):
+        """ Returns the highest-value classification for a hand and sets its label accordingly. """
+        if self.has_straight_flush():
+            self.label = 'straight flush'
+        elif self.has_four_of_a_kind():
+            self.label = 'four of a kind'
+        elif self.has_full_house():
+            self.label = 'full house'
+        elif self.has_flush():
+            self.label = 'flush'
+        elif self.has_straight():
+            self.label = 'straight'
+        elif self.has_three_of_a_kind():
+            self.label = 'three of a kind'
+        elif self.has_two_pair():
+            self.label = 'two pair'
+        elif self.has_pair():
+            self.label = 'pair'
+
+
+label_hist = {}
+
+
+def classy_count(deck, hands, cards_in_hand):
+    """ Shuffles a deck of cards, deals the hands, and counts their labels. """
+
+    deck.shuffle()
+    for i in range(hands):                  # deals the cards and classify the hands
         hand = PokerHand()
-        deck.move_cards(hand, 7)
+        deck.move_cards(hand, cards_in_hand)
         hand.sort()
-        print(hand)
-        print('---------------------------')
-        print(f'       Pair?: {hand.has_pair()}')
-        print(f'     2-Pair?: {hand.has_two_pair()}')
-        print(f'3-of-a-kind?: {hand.has_three_of_a_kind()}')
-        print(f'      Flush?: {hand.has_flush()}')
-        print(f'   Straight?: {hand.has_straight()}')
-        print('')
+        # print(hand)
+        # print('---------------------------')
+        # print(f'          Pair?: {hand.has_pair()}')
+        # print(f'        2-Pair?: {hand.has_two_pair()}')
+        # print(f'   3-of-a-kind?: {hand.has_three_of_a_kind()}')
+        # print(f'      Straight?: {hand.has_straight()}')
+        # print(f'         Flush?: {hand.has_flush()}')
+        # print(f'    Full House?: {hand.has_full_house()}')
+        # print(f'   4-of-a-kind?: {hand.has_four_of_a_kind()}')
+        # print(f'Straight Flush?: {hand.has_straight_flush()}')
+        hand.classify()
+        # print(f'  Highest value: {hand.label}')
+        # print('')
+        if hand.label:
+            label_hist[hand.label] = label_hist.get(hand.label, 0) + 1
+    # print('-----------------------')
+    # for item in label_hist:
+    #     print(f'{item}: {label_hist[item]}')
+
+
+def print_table(histogram):
+    """ Prints a histogram of classifications in descending order of frequency and its probability. """
+
+    class_by_value = sorted(label_hist, key=label_hist.get, reverse=True)   # sorting the keys in decreasing freq
+
+    print("{:<20} {:<10} {:<10}".format('hand', 'freq', 'probs'))
+    for hand in class_by_value:
+        hits = label_hist[hand]
+        probs = hits / 1000
+        print("{:<20} {:<10} {:<10}".format(hand, hits, probs))
+
+
+for _ in range(1000):
+    # making a deck
+    deck = Deck()
+    classy_count(deck, 7, 7)
+
+print_table(label_hist)
